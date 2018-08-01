90d49c757e404e1716a3d269bc27f0e9350befe0
zeagord <sundarsy@gmail.com>
2018-08-01 13:05:01 +0700

    Upgrades to Springboot2:

--- springcloudgateway/src/test/scala/springcloudgateway/GatlingTest.scala
+++ springcloudgateway/src/test/scala/springcloudgateway/GatlingTest.scala
@@ -1,20 +1,25 @@
 package com.bytesville.microservices.demo.springcloudgateway
 
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
-
 class GatlingTest extends Simulation {
-
   val httpConf = http
     .baseURL("http://localhost:8080")
-    //.headers(Map("auth"->"someauth"))// Here is the root for all relative URLs
-
   val scn = scenario("Sample")
     .exec(http("GetIP")
       .get("/getip"))
-    .pause(2 milliseconds)
-  setUp(scn.inject(rampUsers(1) over(5 seconds)) .protocols(httpConf))
+    .pause(100 milliseconds)
+  setUp(scn.inject(rampUsers(50000) over(60 seconds)) .protocols(httpConf))
 }
+
+//class GatlingTest extends Simulation {
+//  val httpConf = http
+//    .baseURL("http://localhost:8765")
+//  val scn = scenario("Sample")
+//    .exec(http("GetIP")
+//      .get("/api/myservice/ip"))
+//    .pause(100 milliseconds)
+//  setUp(scn.inject(rampUsers(5000) over(60 seconds)) .protocols(httpConf))
+//}
